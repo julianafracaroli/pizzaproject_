85e4beb700576a135e8afa6a9a0625b9835d2b4d
Aluno <julianacfracaroli@gmail.com>
2019-08-05 12:10:38 -0300

    a dilma foi impet

--- telalocalhr.php
+++ telalocalhr.php
@@ -11,15 +11,15 @@
   <div class="container border shadow-md fundo-pizza p-5">
     <div class="row py-5" >
       <div class="col-md py-5">
-            <div class="bg-danger text-center mx-auto p-2" style="width: 400px;"  >
-                <div class="text-white rounded mx-auto text-center mt-5 display-1 font-weight-normal " style="width: 250px;">
-          <h1>Local<h1/>
-            <h2>E Horários</h2>
+            <div class=" text-center mx-auto p-2" style="width: 500px; background-color: #FFFFFF55;"  >
+                <div class="text-danger rounded mx-auto text-center mt-5 display-1 font-weight-normal " >
+                <h1>Local<h1/>
+                  <h2>E Horários</h2>
 
-          <div class="dados text-dark">
-            <h3><img src="img/casa.png" alt="">Rua Paraiba, 34</h3>
-            <h3><img src="img/telefone.png" alt="">44|3423-6538</h3>
-            <h3><img src="img/email.png" alt="">pizzaplanet@gmail.com</h3
+                <div class="dados text-dark">
+                  <h3><img src="img/casa.png" width="28" alt="">Rua Paraiba, 34</h3>
+                  <h3><img src="img/telefone.png" width="28" alt="">44|3423-6538</h3>
+                  <h3><img src="img/email.png" width="28" alt="">pizzaplanet@gmail.com</h3>
 
 
             </div>
--- telamenu.php
+++ telamenu.php
@@ -6,9 +6,66 @@
 <body class="">
 
   <?php include('menu.php') ?>
-
   <div class="container border shadow-md fundo-pizza p-5">
     <div class="row py-5" >
       <div class="col-md py-5">
+        <div class=" mx-auto p-2" style="width: 500px; background-color: #FFFFFF77;"  >
+          <div class="text-danger rounded mx-auto  mt-5 display-1 font-weight-normal " >
+            <h1>Pizzas Salgadas</h1>
 
-        <div class="text-white rounded mx-auto text-center mt-5 display-1 " style="width: 250px;">
+            <div class="pizzas text-dark">
+              <h4><li><strong>ATUM<strong><h4>
+
+                <h5>Molho de tomate, mussarela, atum, tomate, azeitonas e orégano (cebola opcional).</li></h5>
+
+                <h4><li><strong>BACON<strong><h4>
+
+                  <h5>Molho de tomate, mussarela, bacon fatiado, azeitonas e orégano.</li></h5>
+
+                  <h4><li><strong>BAIANA<strong><h4>
+
+                    <h5>Molho de carne seca apimentado, mussarela, pimentão, cebola, pimenta, azeitonas e orégano.</li></h5>
+
+                    <h4><li><strong>CALABRESA<strong><h4>
+
+                          <h5>Molho de tomate, mussarela, calabresa fatiada, azeitonas e orégano (cebola opcional).</li></h5>
+
+                    <h4><li><strong>CARBONARA<strong><h4>
+
+                      <h5>Molho de tomate, ovos, mussarela, parmesão, bacon fatiado, azeitonas e orégano.</li></h5>
+
+                      <h4><li><strong>FRANGO COM BACON<strong><h4>
+
+                           <h5>Molho de tomate, frango desfiado, mussarela, cheddar, azeitonas e orégano.
+</li></h5>
+
+<h4><li><strong>FRANGO COM CATUPIRY<strong><h4>
+
+     <h5>Molho de tomate, frango desfiado, mussarela, requeijão, azeitonas e orégano.
+</li></h5>
+
+  <div class="pizzasdireita text-dark">
+    <h4><li><strong>FRANGO COM CHEDDAR<strong><h4>
+
+         <h5>Molho de tomate, frango desfiado, mussar ela, cheddar, azeitonas e orégano
+    </li></h5>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>

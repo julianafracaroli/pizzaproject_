c45d507f31f4e3d565fa26e3e0af2bc3939e738d
54 <julianacfracaroli@gmail.com>
2019-08-15 12:01:38 -0300

    Add files via upload

--- telamenu.php
+++ telamenu.php
@@ -7,13 +7,12 @@
 
   <?php include('menu.php') ?>
   <div class="container border shadow-md fundo-pizza p-5">
-    <div class="row py-5" >
-      <div class="col-md py-5">
-        <div class=" mx-auto p-2" style="width: 500px; background-color: #FFFFFF77;"  >
-          <div class="text-danger rounded mx-auto  mt-5 display-1 font-weight-normal " >
+    <div class="row py-5" style="width: 500px; background-color: #FFFFFF77;">
+        <div class=" mx-auto p-2">
+          <div class="text-danger rounded mx-auto  mt-5 display-5 font-weight-normal " >
             <h1>Pizzas Salgadas</h1>
 
-            <div class="pizzas text-dark">
+            <div class="pizza text-dark">
               <h4><li><strong>ATUM<strong><h4>
 
                 <h5>Molho de tomate, mussarela, atum, tomate, azeitonas e orégano (cebola opcional).</li></h5>
@@ -43,29 +42,67 @@
 
      <h5>Molho de tomate, frango desfiado, mussarela, requeijão, azeitonas e orégano.
 </li></h5>
-
-  <div class="pizzasdireita text-dark">
-    <h4><li><strong>FRANGO COM CHEDDAR<strong><h4>
-
-         <h5>Molho de tomate, frango desfiado, mussar ela, cheddar, azeitonas e orégano
-    </li></h5>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                    </div>
                   </div>
                 </div>
               </div>
             </div>
+          </div>
+
+            <div class="row py-5" style="width: 500px; background-color: #FFFFFF77;position:absolute;top:140px;left:700px;">
+                <div class=" mx-auto p-2">
+                  <div class="text-danger rounded mx-auto  mt-5 display-1 font-weight-normal " >
+                    <h1>Pizzas Salgadas</h1>
+
+          <div class="pizzasdireita text-dark">
+            <h4><li><strong>FRANGO COM CHEDDAR<strong><h4>
+
+                 <h5>Molho de tomate, frango desfiado, mussar ela, cheddar, azeitonas e orégano
+            </li></h5>
+
+
+            <h4><li><strong>PALMITO<strong><h4>
+
+                 <h5>Molho de tomate, palmito, mussarela, ervilha, azeitonas e orégano.
+            </li></h5>
+
+            <h4><li><strong>PIZZA CODORNINHA<strong><h4>
+
+                 <h5>Molho de tomate, ovo de codorna, salame, azeitona, mussarela e orégano.
+            </li></h5>
+
+            <h4><li><strong>PORTUGUESA<strong><h4>
+
+                 <h5>Molho de tomate, presunto, cebola, ervilha, ovo, batata, mussarela e orégano.
+            </li></h5>
+
+            <h4><li><strong>STROGONOFF DE CARNE<strong><h4>
+
+                 <h5>Molho de tomate, strogonoff de carne, batata palha, mussarela e orégano.
+            </li></h5>
+
+            <h4><li><strong>STROGONOFF DE FRANGO<strong><h4>
+
+                 <h5>Molho de tomate, strogonoff de frango, batata palha, mussarela e orégano.
+            </li></h5>
+
+            <h4><li><strong>VEGETARIANA<strong><h4>
+
+                 <h5>Molho de tomate, palmito, milho, brócolis, mussarela, azeitonas e orégano.
+            </li></h5>
+
+
+
+
+
+
+
+
+
+
+
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                  </div>

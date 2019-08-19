fc066bfc9b45f2f7d0f5535d21f83cf1bc3649e2
54 <julianacfracaroli@gmail.com>
2019-08-19 11:26:05 -0300

    Add files via upload

---
+++ telamenudoce.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <?php include('head.php') ?>
+    <meta charset="utf-8">
+    <title>doce</title>
+  </head>
+  <body>
+    <?php include('menu.php') ?>
+    <div class="container border shadow-md fundo-pizza p-5">
+      <div class="row py-5" style="width: 500px; background-color: #FFFFFF77;">
+          <div class=" mx-auto p-2">
+            <div class="text-danger rounded mx-auto  mt-5 display-5 font-weight-normal " >
+              <h1>Pizzas Doces</h1>
+
+              <div class="pizza text-dark">
+                <h4><li><strong>Chocolate<strong><h4>
+
+                  <h5> mussarela e chocolate ao leite.</li></h5>
+
+                  <h4><li><strong>cholate branco<strong><h4>
+
+                    <h5>mussarela e chocolate branco.</li></h5>
+
+                    <h4><li><strong>chocolate meio amargo<strong><h4>
+
+                      <h5>mussarela e chocolate meio amargo.</li></h5>
+
+                      <h4><li><strong>banana com canela<strong><h4>
+
+                            <h5>mussarela, banana e pó de canela.</li></h5>
+
+                      <h4><li><strong>romeu e julieta<strong><h4>
+
+                        <h5>mussarela, queijo branco e goiabada.</li></h5>
+
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+  </div>
+              <div class="row py-5" style="width: 500px; background-color: #FFFFFF77;position:absolute;top:140px;left:700px;">
+                  <div class=" mx-auto p-2">
+                    <div class="text-danger rounded mx-auto  mt-5 display-1 font-weight-normal " >
+                      <h1>Pizzas Salgadas</h1>
+
+            <div class="pizzasdireita text-dark">
+              <h4><li><strong>bombom<strong><h4>
+
+                   <h5>mussarela, chocolate ao leite e bombons.
+              </li></h5>
+
+
+              <h4><li><strong>confete<strong><h4>
+
+                   <h5>chocolate ao leite, mussarela e confetes.
+              </li></h5>
+
+              <h4><li><strong>casadinho<strong><h4>
+
+                   <h5>chocolate ao leite, chocolate branco e mussarela.
+              </li></h5>
+
+
+              <h4><li><strong>sensação branca<strong><h4>
+
+             <h5>chocolate branco, mussarela e morangos.</li></h5>
+
+                <h4><li><strong>sensação<strong><h4>
+
+                     <h5>chocolate ao leite, mussarela e morangos</li></h5>
+
+
+
+
+
+
+
+
+
+
+
+                              </div>
+                            </div>
+                          </div>
+                        </div>
